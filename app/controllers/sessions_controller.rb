ebe870d3ed4655052650675089c8e5d1b8039b39
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-11-03 22:34:35 -0800

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        if params[:password] == nil || params[:password].empty? 
+            redirect_to '/login'
+        elsif @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to '/welcome'
+        end
+    end
+end
